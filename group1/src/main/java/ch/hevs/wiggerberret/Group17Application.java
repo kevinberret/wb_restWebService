578cd761b143967048b920d8e203f58f0bb80e5f
kevinberret <berret.kevin@gmail.com>
2017-06-08 16:40:33 +0200

    modifs pour war file

--- group1/src/main/java/ch/hevs/wiggerberret/Group17Application.java
+++ group1/src/main/java/ch/hevs/wiggerberret/Group17Application.java
@@ -2,13 +2,20 @@ package ch.hevs.wiggerberret;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /*
  * MAIN Webservice Application
  */
 @SpringBootApplication
-public class Group17Application {
+public class Group17Application extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Group17Application.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Group17Application.class, args);
 	}
