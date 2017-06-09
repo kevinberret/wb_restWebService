67bb3dc937956f1d388674b67fc6cb9adf47be8e
kevinberret <berret.kevin@gmail.com>
2017-06-09 14:08:01 +0200

    .

--- group1/src/main/java/ch/hevs/wiggerberret/Group17Application.java
+++ group1/src/main/java/ch/hevs/wiggerberret/Group17Application.java
@@ -2,20 +2,13 @@ package ch.hevs.wiggerberret;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /*
  * MAIN Webservice Application
  */
 @SpringBootApplication
-public class Group17Application extends SpringBootServletInitializer {
+public class Group17Application {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(Group17Application.class);
-	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(Group17Application.class, args);
 	}
